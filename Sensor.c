23cf3d07efb07cadb50867fc8f7ea7a994daaa8a
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2021-02-19 15:20:36 +0000

    added joystick

--- Sensor.c
+++ Sensor.c
@@ -5,6 +5,38 @@
 
 #define		PCF     120
 #define		DOpin	0
+#define uchar	unsigned char
+
+int AIN0 = PCF + 0;
+int AIN1 = PCF + 1;
+int AIN2 = PCF + 2;
+
+char *state[7] = {"home", "up", "down", "left", "right", "pressed"};
+
+int direction(){
+	int x, y, b;
+	int tmp=0;
+	x = analogRead(AIN1);
+	y = analogRead(AIN0);
+	b = analogRead(AIN2);
+	if (y <= 30)
+		tmp = 1;		// up
+	if (y >= 225)
+		tmp = 2;		// down
+	
+	if (x >= 225)
+		tmp = 3;		// left
+	if (x <= 30)
+		tmp = 4;		// right
+
+	if (b <= 30)
+		tmp = 5;		// button preesd
+	if (x-125<15 && x-125>-15 && y-125<15 && y-125>-15 && b >= 60)
+		tmp = 0;		// home position
+	
+	return tmp;
+}
+
 
 void PrintTemp(int x)
 {
@@ -54,8 +86,7 @@ int main()
 {
 	unsigned char analogVal, analogVal1;
 	double Vr, Rt, temp;
-	int tmp, status, tmp1, status1;
-	
+	int tmp, status, tmp1, status1,tmp1,status2;
 	if(wiringPiSetup() == -1){
 		printf("setup wiringPi failed !");
 		return 1;
@@ -68,6 +99,13 @@ int main()
 	status = 0;
 	while(1) // loop forever
 	{
+		tmp2 = direction();
+		if (tmp2 != status2)
+		{
+			printf("%s\n", state[tmp2]);
+			status2 = tmp2;
+		}
+		
 		analogVal = analogRead(PCF + 0);
 		Vr = 5 * (double)(analogVal) / 255;
 		Rt = 10000 * (double)(Vr) / (5 - (double)(Vr));
