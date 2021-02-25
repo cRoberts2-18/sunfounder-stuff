ce757a9d4a452e5f10fedf7b3662efe0f9b7a28b
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2021-02-25 18:43:20 +0000

    added lcd functionality

--- Sensor.c
+++ Sensor.c
@@ -2,6 +2,8 @@
 #include <wiringPi.h>
 #include <pcf8591.h>
 #include <math.h>
+#include <wiringPiI2C.h>
+#include <string.h>
 
 #define		PCF     120
 #define		DOpin	0
@@ -11,7 +13,92 @@ int AIN0 = PCF + 0;
 int AIN1 = PCF + 1;
 int AIN2 = PCF + 2;
 
+int LCDAddr = 0x27;
+int BLEN = 1;
+int fd;
+
 char *state[7] = {"home", "up", "down", "left", "right", "pressed"};
+void write_word(int data){
+	int temp = data;
+	if ( BLEN == 1 )
+		temp |= 0x08;
+	else
+		temp &= 0xF7;
+	wiringPiI2CWrite(fd, temp);
+}
+
+void send_command(int comm){
+	int buf;
+	// Send bit7-4 firstly
+	buf = comm & 0xF0;
+	buf |= 0x04;			// RS = 0, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+
+	// Send bit3-0 secondly
+	buf = (comm & 0x0F) << 4;
+	buf |= 0x04;			// RS = 0, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+}
+
+void send_data(int data){
+	int buf;
+	// Send bit7-4 firstly
+	buf = data & 0xF0;
+	buf |= 0x05;			// RS = 1, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+
+	// Send bit3-0 secondly
+	buf = (data & 0x0F) << 4;
+	buf |= 0x05;			// RS = 1, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+}
+
+void init(){
+	send_command(0x33);	// Must initialize to 8-line mode at first
+	delay(5);
+	send_command(0x32);	// Then initialize to 4-line mode
+	delay(5);
+	send_command(0x28);	// 2 Lines & 5*7 dots
+	delay(5);
+	send_command(0x0C);	// Enable display without cursor
+	delay(5);
+	send_command(0x01);	// Clear Screen
+	wiringPiI2CWrite(fd, 0x08);
+}
+
+void clear(){
+	send_command(0x01);	//clear Screen
+}
+
+void write(int x, int y, char data[]){
+	int addr, i;
+	int tmp;
+	if (x < 0)  x = 0;
+	if (x > 15) x = 15;
+	if (y < 0)  y = 0;
+	if (y > 1)  y = 1;
+
+	// Move cursor
+	addr = 0x80 + 0x40 * y + x;
+	send_command(addr);
+	
+	tmp = strlen(data);
+	for (i = 0; i < tmp; i++){
+		send_data(data[i]);
+	}
+}
 
 int direction(){
 	int x, y, b;
@@ -154,8 +241,14 @@ int main()
 				PrintRain(tmp);
 				status = tmp;
 			}
-
-			delay (200);
+			
+		
+			fd = wiringPiI2CSetup(LCDAddr);
+			init();
+			write(0, 0, "Greetings!");
+			write(1, 1, "From SunFounder");
+			delay(2000);
+		
 			pass=0;
 	}
 	
