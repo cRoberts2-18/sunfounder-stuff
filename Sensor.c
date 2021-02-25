c3875e861ab107d338de97f7f0ec42286edf0099
cRoberts2-18 <75317368+cRoberts2-18@users.noreply.github.com>
2021-02-25 18:59:49 +0000

    Update Sensor.c

--- Sensor.c
+++ Sensor.c
@@ -222,6 +222,7 @@ int main()
 			if(pass==0){
 				if(round(temp*100)!=round(temperature*100)){
 				printf("Current temperature : %lf\n", temp);
+				printf(temperature);
 				temperature=temp;
 				}
 			}
